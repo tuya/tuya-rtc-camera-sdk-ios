0fbb62b18ad3a4c84bf45399c5b05a4b30748a7c
weifeng <weifeng.huang@tuya.com>
2021-07-03 14:14:34 +0800

    update the doc.

--- doc/html/search/all_2.js
+++ doc/html/search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['getbuildtime_5',['getBuildTime',['../interface_tuya_r_t_c_engine.html#a3f2250810b29b7f7538de881827b276b',1,'TuyaRTCEngine']]],
-  ['getremoteaudiomute_6',['getRemoteAudioMute',['../interface_tuya_r_t_c_camera.html#a078347e792da7f7910d39296f87ad1b3',1,'TuyaRTCCamera']]],
-  ['getremotevideomute_7',['getRemoteVideoMute',['../interface_tuya_r_t_c_camera.html#a6e15fb4d914424361ca7dda631d3ee19',1,'TuyaRTCCamera']]],
-  ['getsdkversion_8',['getSdkVersion',['../interface_tuya_r_t_c_engine.html#a524f7af02aa72d7f5362e7139b15f402',1,'TuyaRTCEngine']]]
+  ['getbuildtime_6',['getBuildTime',['../interface_tuya_r_t_c_engine.html#a3f2250810b29b7f7538de881827b276b',1,'TuyaRTCEngine']]],
+  ['getremoteaudiomute_7',['getRemoteAudioMute',['../interface_tuya_r_t_c_camera.html#a078347e792da7f7910d39296f87ad1b3',1,'TuyaRTCCamera']]],
+  ['getremotevideomute_8',['getRemoteVideoMute',['../interface_tuya_r_t_c_camera.html#a6e15fb4d914424361ca7dda631d3ee19',1,'TuyaRTCCamera']]],
+  ['getsdkversion_9',['getSdkVersion',['../interface_tuya_r_t_c_engine.html#a524f7af02aa72d7f5362e7139b15f402',1,'TuyaRTCEngine']]]
 ];
--- doc/html/search/classes_0.js
+++ doc/html/search/classes_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['tuyartccamera_25',['TuyaRTCCamera',['../interface_tuya_r_t_c_camera.html',1,'']]],
-  ['tuyartccameradelegate_2dp_26',['TuyaRTCCameraDelegate-p',['../protocol_tuya_r_t_c_camera_delegate-p.html',1,'']]],
-  ['tuyartcengine_27',['TuyaRTCEngine',['../interface_tuya_r_t_c_engine.html',1,'']]],
-  ['tuyartcenginedelegate_2dp_28',['TuyaRTCEngineDelegate-p',['../protocol_tuya_r_t_c_engine_delegate-p.html',1,'']]]
+  ['tuyartccamera_26',['TuyaRTCCamera',['../interface_tuya_r_t_c_camera.html',1,'']]],
+  ['tuyartccameradelegate_2dp_27',['TuyaRTCCameraDelegate-p',['../protocol_tuya_r_t_c_camera_delegate-p.html',1,'']]],
+  ['tuyartcengine_28',['TuyaRTCEngine',['../interface_tuya_r_t_c_engine.html',1,'']]],
+  ['tuyartcenginedelegate_2dp_29',['TuyaRTCEngineDelegate-p',['../protocol_tuya_r_t_c_engine_delegate-p.html',1,'']]]
 ];
